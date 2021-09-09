aa8ca0d8cfc2b4475cc474ffaabc310271bfb43d
EverthonH <Everthon.henrique.75@gmail.com>
2021-09-09 09:00:33 -0300

    Implementação do breeze

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
- Route::get('/livros', function(){
-     return view('livros');
- });
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+require __DIR__.'/auth.php';
